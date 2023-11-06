37096c65cc9e06b8cb86bdf70b819bf722eacd0d
Aaliyah Reid <aaliyahreid12345@gmail.com>
2023-11-05 20:12:07 -0500

    handles user submitting email id

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function(){
+
+    const form = document.querySelector("form");
+    const emailID = document.getElementById("email");
+    const messageDisplayed = document.querySelector(".message");
+
+    form.addEventListener("submit",function(event){
+        event.preventDefault();
+        const email = emailID.value.trim();
+
+        if(email=== ""){
+            messageDisplayed.textContent =`Please enter a valid email address.`;
+        } else {
+            messageDisplayed.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    });
+ 
+
+});
